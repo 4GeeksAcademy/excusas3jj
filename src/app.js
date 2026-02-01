fec6666a96ca7e6d8fa730b1ec485870df211cfa
KitsuneRoyale <jjoselopeza777@gmail.com>
2026-02-01 20:32:47 +0000

    porfin

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const randomFrom = arr => arr[Math.floor(Math.random()* arr.length)];
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function generar() {
+  const quien= randomFrom(who);
+  const accion = randomFrom(action);
+  const que = randomFrom(what);
+  const cuando = randomFrom(when);
+
+  return `${quien} ${accion} ${que} ${cuando}.`;
+}
+
+const excuseElement = document.querySelector('.Excusa');
+const boton = document.querySelector('#boton');
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  excuseElement.textContent=generar() 
+  console.log(generar())
 };
+
+
+boton.addEventListener('click', () => {
+  
+  excuseElement.textContent = generar();
+  console.log(generar)
+
+});
+
+ 
